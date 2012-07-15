944096e814e01c4e99d44a84c9df0b3decbbddb2
Vikhyat Korrapati <vikhyatk@gmail.com>
2012-07-15 18:36:13 +0530

    tests for the terminology change of generate to split.

--- tests/stormycloud_spec.rb
+++ tests/stormycloud_spec.rb
@@ -25,23 +25,23 @@ describe StormyCloud do
     end
   end
 
-  describe "#generate" do
+  describe "#split" do
     before(:each) do
       @sc = StormyCloud.new("127.0.0.1")
     end
     
-    it "should raise NotImplementedError if no generate function is given" do
-      expect { @sc.generate }.to raise_error(NotImplementedError)
+    it "should raise NotImplementedError if no split function is given" do
+      expect { @sc.split }.to raise_error(NotImplementedError)
     end
 
     it "should accept a block and save it" do
-      @sc.generate { [1,2,3] }
-      @sc.generate.should == [1,2,3]
+      @sc.split { [1,2,3] }
+      @sc.split.should == [1,2,3]
     end
 
     it "should raise an error when the returned value is not an array" do
-      @sc.generate { "not an array" }
-      expect { @sc.generate }.to raise_error(TypeError)
+      @sc.split { "not an array" }
+      expect { @sc.split }.to raise_error(TypeError)
     end
   end
 
