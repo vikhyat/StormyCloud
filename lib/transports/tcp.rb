6da3569604dfcc85d9aafa8a6de8b6fd66765035
Vikhyat Korrapati <vikhyatk@gmail.com>
2012-07-17 16:14:47 +0530

    tcp transport initial implementation.

--- lib/transports/tcp.rb
+++ lib/transports/tcp.rb
@@ -3,11 +3,25 @@ require 'socket'
 
 class StormyCloudTCPTransport < StormyCloudTransport
   def initialize_server
+    @server = TCPServer.new @stormy_cloud.config(:port)
+    @server_thread = Thread.new do
+      loop do
+        Thread.start(@server.accept) do |client|
+          client.puts handle(client.gets)
+        end
+      end
+    end
   end
 
   def kill_server
+    @server_thread.kill
   end
 
   def raw_send_message(string)
+    s = TCPSocket.new(@stormy_cloud.server, @stormy_cloud.config(:port))
+    s.puts string
+    res = s.gets
+    s.close
+    res
   end
 end
