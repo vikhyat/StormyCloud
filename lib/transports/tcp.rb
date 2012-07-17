cf2958cbea7afeb1f6a0f00e15bd6ed2966bf7d8
Vikhyat Korrapati <vikhyatk@gmail.com>
2012-07-17 15:53:34 +0530

    fleshing out the tcp transport.

--- lib/transports/tcp.rb
+++ lib/transports/tcp.rb
@@ -1,4 +1,13 @@
 require_relative "../transport.rb"
+require 'socket'
 
 class StormyCloudTCPTransport < StormyCloudTransport
+  def initialize_server
+  end
+
+  def kill_server
+  end
+
+  def raw_send_message(string)
+  end
 end
--- tests/tcp_transport_spec.rb
+++ tests/tcp_transport_spec.rb
@@ -1,4 +1,9 @@
 require_relative "../lib/transports/tcp.rb"
 
 describe StormyCloudTCPTransport do
+  describe "#run" do
+    sc = StormyCloud.new("test", "localhost")
+    t = StormyCloudTCPTransport.new(sc)
+    p t.run
+  end
 end
